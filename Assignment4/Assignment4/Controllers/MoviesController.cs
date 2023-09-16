60ec499101ea0a24905cc7e650f030e031980774
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-09-16 21:11:50 +0530

    Assignment

---
+++ Assignment4/Assignment4/Controllers/MoviesController.cs
@@ -0,0 +1,94 @@
+﻿using Assignment4.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>
+        {
+            new Movie
+            {
+                Id = 1,
+                Title = "RRR",
+                Genre = "Action",
+                ReleaseYear = 2021,
+                Director = "SSR",
+                Actors = "Ram Charan, NTR, Alia, Ajaj Devgan"
+            },
+            new Movie
+            {
+                Id = 2,
+                Title = "John Wick",
+                Genre = "Action, Thriller",
+                ReleaseYear = 2014,
+                Director = "Chad Stahelski",
+                Actors = "Keanu Reeves, Michael Nyqvist"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetMovies()
+        {
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Movie> GetMovie(int id)
+        {
+            var movie = movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Movie> PutMovie(int id, Movie updatedMovie)
+        {
+            var existingMovie = movies.FirstOrDefault(m => m.Id == id);
+            if (existingMovie == null)
+            {
+                return NotFound();
+            }
+
+            existingMovie.Title = updatedMovie.Title;
+            existingMovie.Genre = updatedMovie.Genre;
+            existingMovie.ReleaseYear = updatedMovie.ReleaseYear;
+            existingMovie.Director = updatedMovie.Director;
+            existingMovie.Actors = updatedMovie.Actors;
+
+            return Ok(existingMovie);
+        }
+
+        [HttpPost]
+        public ActionResult<Movie> PostMovie(Movie newMovie)
+        {
+            var maxId = movies.Max(m => m.Id);
+            newMovie.Id = maxId + 1;
+
+            movies.Add(newMovie);
+
+            return CreatedAtAction(nameof(GetMovie), new { id = newMovie.Id }, newMovie);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteMovie(int id)
+        {
+            var movieToRemove = movies.FirstOrDefault(m => m.Id == id);
+            if (movieToRemove == null)
+            {
+                return NotFound();
+            }
+
+            movies.Remove(movieToRemove);
+
+            return NoContent();
+        }
+    }
+}
